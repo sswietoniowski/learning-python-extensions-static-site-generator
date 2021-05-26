191b5111582789f7d0874c405e21a4e457ae2dec
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-05-26 16:49:14 +0200

    Load Extensions

---
+++ ssg/extensions.py
@@ -0,0 +1,13 @@
+import sys
+import importlib
+from pathlib import Path
+
+def load_module(directory, name):
+    sys.path.insert(0, directory)
+    importlib.import_module(name)
+    sys.path.pop(0)
+
+
+def load_directory(directory: Path):
+    for path in directory.rglob(".py"):
+        load_module(directory.as_posix(), path.stem)
